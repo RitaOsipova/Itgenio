a2c27bafc9a5f53b88c427ee628c84d0d9886caa
RitaOsipova <margarita.osipova@web.de>
2021-09-02 16:25:48 +0300

    For tasks

--- main.py
+++ main.py
@@ -4,8 +4,8 @@
 #########################turtle############################
 
 
-import turtle
-t = turtle.Pen()
+#import turtle
+# t = turtle.Pen()
 # t.down()
 # t.forward(10)
 # t.up()
@@ -127,7 +127,7 @@ t = turtle.Pen()
 # a = input()
 
 
-#labyrinth
+# labyrinth
 # t.forward(100)
 # t.left(90)
 # t.forward(95)
@@ -170,38 +170,209 @@ t = turtle.Pen()
 # t.left(90)
 # a = input()
 
-t.forward(100)
-t.left(90)
-t.forward(200)
-t.left(90)
-t.forward(100)
-t.left(90)
-t.forward(200)
-t.left(90)
-t.forward(400)
-t.left(90)
-t.forward(200)
-t.right(90)
-t.forward(100)
-t.right(90)
-t.forward(200)
-t.right(90)
-t.forward(100)
-t.right(90)
-t.forward(150)
-t.left(90)
-t.forward(300)
-t.right(90)
-t.forward(50)
-t.left(45)
-t.forward(80)
-t.left(98)
-t.forward(80)
-t.up()
-t.goto(100,150)
-t.down()
-t.right(180)
-t.forward(215)
-t.right(95)
-t.forward(215)
-a = input()
+# t.forward(100)
+# t.left(90)
+# t.forward(200)
+# t.left(90)
+# t.forward(100)
+# t.left(90)
+# t.forward(200)
+# t.left(90)
+# t.forward(400)
+# t.left(90)
+# t.forward(200)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(200)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(150)
+# t.left(90)
+# t.forward(300)
+# t.right(90)
+# t.forward(50)
+# t.left(45)
+# t.forward(80)
+# t.left(98)
+# t.forward(80)
+# t.up()
+# t.goto(100,150)
+# t.down()
+# t.right(180)
+# t.forward(215)
+# t.right(95)
+# t.forward(230)
+# t.goto(400,200)
+# t.left(90)
+# t.forward(75)
+# t.right(95)
+# t.forward(85)
+# a = input()
+
+
+###########################################################
+###########################################################
+###########################################################
+
+# 1).
+# string = '4###5abn^RrRy%5^aaAn9@444GCdDD%M'
+# length = len(string)
+# counter = 0
+# for i in range(length-1):
+#     string = string.lower()
+#     if string[i] == string[i + 1] == string[i + 2]:
+#         counter += 1
+# print(f'Es gibt {counter} gleiche paare')
+
+# 2).
+# string = 'hello world!'
+# string = string[::-1]
+# print(string.rstrip('h'))
+###########################################################
+###########################################################
+###########################################################
+
+# print('-------------------- 8 ---------------------')
+# print('------------------- 8 8 --------------------')
+# print('------------------ 8 8 8 -------------------')
+# print('----------------- 8 8 8 8 ------------------')
+# print('---------------- 8 8 8 8 8 -----------------')
+# print('--------------- 8 8 8 8 8 8 ----------------')
+# print('-------------- 8 8 8 8 8 8 8 ---------------')
+# print('------------- 8 8 8 8 8 8 8 8 --------------')
+# print('------------ 8 8 8 8 8 8 8 8 8 -------------')
+# print('----------- 8 8 8 8 0 0 8 8 8 8 ------------')
+# print('---------- 8 8 8 8 0 0 0 8 8 8 8 -----------')
+# print('--------- 8 8 8 8 8 0 0 8 8 8 8 8 ----------')
+# print('-------- 8 8 8 8 8 8 8 8 8 8 8 8 8 ---------')
+# print('------- 8 8 8 8 8 8 8 8 8 8 8 8 8 8 --------')
+# print('------ 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 -------')
+# print('----- 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 ------')
+# print('---- 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 -----')
+# print('--- 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 ----')
+# print('--- 00000000000000000000000000000000000 ----')
+# print('--- 00000000000000000000000000000000000 ----')
+# print('--- 00000000000000000000000000000000000 ----')
+# print('--- 00000000000000000000000000000000000 ----')
+# print('--- 00000000000000000000000000000000000 ----')
+# print('--- 00000000000000000000000000000000000 ----')
+# print('--- 00000000000000000000000000000000000 ----')
+# print('--- 00000000000000000000000000000000000 ----')
+# print('--- 00000000000000000000000000000000000 ----')
+# print('--- IIIIIIIIIIII00000000000000000000000 ----')
+# print('--- I0000000000I00000000000000000000000 ----')
+# print('--- I0000000000I00000000000000000000000 ----')
+# print('--- I0000000000I00000000000000000000000 ----')
+# print('--- I&000000000I00000000000000000000000 ----')
+# print('--- I0000000000I00000000000000000000000 ----')
+# print('--- I0000000000I00000000000000000000000 ----')
+
+###########################################################
+###########################################################
+###########################################################
+
+# 1 + 2####################################################
+# star = '⭐️'
+# for i in range(0, 6):
+#     print(star, end=' ')
+# print(end='\n') 
+# for i in range(0, 21):
+#     print(star, end=' ')
+# print(end='\n')
+# 3 #######################################################
+# star = '⭐️'
+# for i in range(0, 11):
+#     print(end='\n')
+#     for i in range(0, 11):
+#         print(star, end=' ')
+# 4 #######################################################
+# star = '⭐️'
+# for i in range(0, 11):
+#     print(end='\n')
+#     for i in range(0, 6):
+#         print(star, end=' ') 
+# 5 #######################################################     
+# star = '⭐️'
+# for i in range(0, 6):
+#     print(end='\n')
+#     for i in range(0, 21):
+#         print(star, end=' ')       
+# 6 #######################################################
+# star = '0 1 2 3 4 5 6 7 8 9'
+# for i in range(0, 11):
+#     print(end='\n')
+#     for i in range(0, 11):
+#         print(i, end=' ')  
+# 7 #######################################################
+# for a in range(0, 11):
+#      print(end='\n')
+#      for i in range(0, 10):
+#          print(a, end=' ') 
+# 8 #######################################################
+# for a in range(0, 11):
+#      print(end='\n')
+#      for i in range(0, a):
+#          print(a, end=' ') 
+# 9 #######################################################
+# for a in range(9, -1, -1):
+#      print(end='\n')
+#      for i in range(0, a):
+#          print(i, end=' ') 
+# 10 ######################################################
+# for a in range(9, -1, -1):
+#     print(end='\n')
+#     for k in range (0, 18-2*a):
+#         print(' ', end=' ')
+#     for i in range(0, a):
+#         print(i, end=' ')
+# 11 ######################################################
+# for r in range(1,10):
+#     for i in range(1, 10):
+#         print(i*r,end='\t')
+#     print(end='\n')
+# 12 ######################################################
+# num = 9
+# for i in range(1, num+1):
+#     for j in range(1, num-i+1):
+#         print(end="  ")
+#     for j in range(1, i):
+#         print(j, end=" ")
+#     for j in range(i, 0, -1):
+#         print(j, end=" ")
+#     print()
+# 13 ######################################################
+# num = 9
+# for m in range(1, num+1):
+#     for j in range(1, num-m+1):
+#         print(end="  ")
+#     for j in range(1, m):
+#         print(j, end=" ")
+#     for j in range(m, 0, -1):
+#         print(j, end=" ")
+#     print()
+# for m in range(num, 1, -1):
+#     for j in range(0, num-m+1):
+#         print(end="  ")
+#     for j in range(1, m):
+#         print(j, end=" ")
+#     print()   
+# 14 ######################################################
+# num = 9
+# for m in range(1, num+1):
+#     for j in range(1, num-m+1):
+#         print(end="  ")
+#     for j in range(1, m):
+#         print(j, end=" ")
+#     for j in range(m, 0, -1):
+#         print(j, end=" ")
+#     print()
+# for m in range(num, 1, -1):
+#     for j in range(0, num-m+1):
+#         print(end="  ")
+#     for j in range(1, m):
+#         print(j, end=" ")
+#     for j in range(m-1, 0, -1):
+#         print(j, end=' ')
+#     print()   
