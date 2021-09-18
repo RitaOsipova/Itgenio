5d51139e3cc1afe699b88928e722a37f6a953716
RitaOsipova <margarita.osipova@web.de>
2021-09-18 19:09:57 +0200

    aichumie projekt

--- main.py
+++ main.py
@@ -245,9 +245,100 @@
 
 #6.########################################################
 
-mylist = input() .split()
-for i in range(0, len(mylist)-1):
-    if int(mylist[i]) > int(mylist[i + 1]):
-        print('Yes')
-    else:
-        print('No')
+# erg = []
+# mylist = input() .split()
+# for i in range(0, len(mylist)-1):
+#     if int(mylist[i]) > int(mylist[i + 1]):
+#         erg.append('Yes')
+#     else:
+#         erg.append('No')
+# if 'No' in erg:
+#     print('No')
+# else:
+#     print('Yes')
+
+#7.########################################################
+
+# mylist = input() .split()
+# erg = input() .split()
+# for i in range(0, len(mylist)):
+#     if mylist[i] in erg:
+#         print(mylist[i])
+
+#8.########################################################
+
+#mylist = input() .split()
+#ans = []
+#for i in range(0, len(mylist)):
+ 
+
+#9.########################################################
+
+# mylist = ['1','8','6','4','12','1','22','15']
+# n = 5
+# m = []
+# mylist.append(n)
+# for a in range(0, len(mylist)):
+#     if int(mylist[a] > mylist[a+1]):
+
+#10.#######################################################
+
+# mylist = input() .split()
+# n = input() .split()
+# if n in mylist:
+#     print('Ja')
+# else:
+#     print('no')
+
+
+###########################################################
+###########################################################
+###########################################################
+#################### alchemie projekt #####################
+#step.1.###################################################
+# import random
+# caldron = ['wasser','lavendel','schipozwet','kaktusmilch']
+# i = 0
+# flask = []
+# for i in range(0, len(caldron)):
+#     flask.append(caldron[i])
+# flask = caldron
+# flask.append(caldron)
+# print(flask)
+# while i < len(caldron):
+#     if caldron[i] != 'wasser' and caldron[i] != 'kaktusmilch':
+#         caldron.pop(i)
+#     else:
+#         i += 1
+# i = 0
+# print(caldron)
+# caldron.append('nüsse')
+# caldron.append('nüsse')
+# for i in range(0, 74):
+#     caldron.append('schischka')
+
+# random.shuffle(caldron)
+# print(caldron)
+# flask2 = random.choice(caldron)
+# print(flask2)
+
+#1.########################################################
+
+# flask3 = []
+# flask3.append(flask)
+# flask3.append(flask2)
+# print(flask3)
+
+#2.########################################################
+
+# while i < len(caldron):
+#     if caldron[i] == 'schischka':
+#         caldron.pop(i)
+#     else:
+#         i += 1
+#     if i == 32:
+#         break
+# print(caldron)
+
+#3.########################################################
+
