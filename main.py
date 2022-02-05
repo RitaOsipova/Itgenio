15a89975da44e5d59a50083243e040279175da08
RitaOsipova <margarita.osipova@web.de>
2022-02-05 19:00:17 +0100

    Discord bot

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
 ##########################################################
-# OTI0MzIxMjM5NTkyNDk3MTky.Ycc3PA.PINH6ibbq-yapReklTrF4bhFEo8
+# 
   
 
-import discord
+# import discord
 
-client = discord.Client()
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name) 
-    print(client.user.id)
-    print('------')
+# client = discord.Client()
+# @client.event
+# async def on_ready():
+#     print('Logged in as')
+#     print(client.user.name) 
+#     print(client.user.id)
+#     print('------')
 
 
-@client.event
-async def on_message(message):
-    channel = message.channel
-    if message.content == '!Hello':
-        await channel.send('Hello, friend!')
+# @client.event
+# async def on_message(message):
+#     channel = message.channel
+#     if message.content == '!Hello' or '!Hi':
+#         await channel.send('Hello, friend!')
 
-client.run('OTI0MzIxMjM5NTkyNDk3MTky.Ycc3PA.PINH6ibbq-yapReklTrF4bhFEo8')
+# client.run('DISCORD_KEY')
+
+n = int(input())
+i = 1
+
+faktoren = []              
+z = n                       
+
+while z > 1:
+  p = 2
+  while p != 0:
+    i += 1
+    p = z % i
+  faktoren.append(i)
+  z = z // i
+
+print(faktoren)
